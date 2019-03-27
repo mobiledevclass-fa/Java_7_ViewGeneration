5cb10074ece90191a33f04e8f08f3c70880f010f
koroteevmv <koroteevmv@gmail.com>
2019-03-27 12:54:15 +0400

    Generated layout added

--- app/src/main/java/com/example/viewgeneration/MainActivity.java
+++ app/src/main/java/com/example/viewgeneration/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.viewgeneration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // создание LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // установим вертикальную ориентацию
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // создаем LayoutParams
+        LayoutParams linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        // устанавливаем linLayout как корневой элемент экрана
+        setContentView(linLayout, linLayoutParam);
     }
 }
