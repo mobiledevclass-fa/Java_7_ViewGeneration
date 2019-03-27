f6c2502f9caf36c28a9ef2c06872b9eedff1e5de
koroteevmv <koroteevmv@gmail.com>
2019-03-27 13:00:57 +0400

    Some elements added

--- app/src/main/java/com/example/viewgeneration/MainActivity.java
+++ app/src/main/java/com/example/viewgeneration/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.viewgeneration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         LayoutParams linLayoutParam = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         // устанавливаем linLayout как корневой элемент экрана
         setContentView(linLayout, linLayoutParam);
+
+        LayoutParams lpView = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
     }
 }
