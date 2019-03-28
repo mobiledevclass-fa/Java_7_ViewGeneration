86465e89449e936c492afc2b944fd9d2ef5d04b9
mvkoroteev <mvkoroteev@fa.ru>
2019-03-28 11:04:19 +0300

    Addition handler created

--- app2/src/main/java/com/example/app2/MainActivity.java
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.app2;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,8 +39,40 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.btnCreate:
+                // Создание LayoutParams c шириной и высотой по содержимому
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                        wrapContent, wrapContent);
+                // переменная для хранения значения выравнивания
+                // по умолчанию пусть будет LEFT
+                int btnGravity = Gravity.LEFT;
 
+                // определяем, какой RadioButton "чекнут" и
+                // соответственно заполняем btnGravity
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+
+                // переносим полученное значение выравнивания в LayoutParams
+                lParams.gravity = btnGravity;
+
+                // создаем Button, пишем текст и добавляем в LinearLayout
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew, lParams);
+
+                break;
+        }
     }
+
 }
 
