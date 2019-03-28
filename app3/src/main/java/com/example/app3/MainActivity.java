72dfcd8ab25592d5209d2203ec3bf885e48e1072
mvkoroteev <mvkoroteev@fa.ru>
2019-03-28 11:15:58 +0300

    Layout created

--- app3/src/main/java/com/example/app3/MainActivity.java
+++ app3/src/main/java/com/example/app3/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.app3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sbWeight = (SeekBar) findViewById(R.id.sbWeight);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+
     }
 }
